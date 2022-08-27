6a026f9adc661b7f5d69e4dd8238d955858ef9ab
rul3r <andersoncjs96@gmail.com>
2022-08-27 18:52:08 -0300

    tcp multithread test funcionou com testes basicos e parece isolar as chamadas. falta testar mais hehe

--- server.cpp
+++ server.cpp
@@ -1,34 +1,81 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <cstdlib>
 #include <iostream>
-#include <boost/asio.hpp>
+#include <thread>
+#include <sstream>
+#include <string>
 
-using namespace boost::asio;
-using ip::tcp;
-using std::string;
-using std::cout;
-using std::endl;
-string read_(tcp::socket & socket) {
-       boost::asio::streambuf buf;
-       boost::asio::read_until( socket, buf, "\n" );
-       string data = boost::asio::buffer_cast<const char*>(buf.data());
-       return data;
-}
-void send_(tcp::socket& socket, const std::string& message) {
-    const std::string msg = message + "\n";
-    boost::asio::write(socket, boost::asio::buffer(message));
-}
+namespace beast = boost::beast;  // from <boost/beast.hpp>
+namespace http = beast::http;    // from <boost/beast/http.hpp>
+namespace net = boost::asio;     // from <boost/asio.hpp>
+using tcp = net::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
+
+static int request_counter = 0;
+
+class RequestHandler {
+   private:
+    tcp::socket socket_;
+
+   public:
+    // Recebe o socket(que é um objeto que foi movido), e move novamente.
+    // Dessa vez move para ser um objeto persistente desse objeto(this)
+    RequestHandler(tcp::socket socket) : socket_(std::move(socket)) {}
+
+    void handle() {
+        std::string message = read_(socket_);
+        std::cout << message << std::endl;
+
+        send_(socket_, "HTTP/1.1 200 OK\r\n\r\nTESTE: " +
+                           std::to_string(request_counter));
+        request_counter++;
+        socket_.close();
+    }
+
+    std::string read_(tcp::socket& socket) {
+        boost::asio::streambuf buf;
+        boost::asio::read_until(socket, buf, "\n");
+        std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+        return data;
+    }
+
+    void send_(tcp::socket& socket, const std::string& message) {
+        const std::string msg = message + "\n";
+        boost::asio::write(socket, boost::asio::buffer(message));
+    }
+};
+
 int main() {
-      boost::asio::io_service io_service;
-    //listen for new connection
-      tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 3005 ));
-    //socket creation 
-      tcp::socket socket_(io_service);
-    //waiting for connection
-      acceptor_.accept(socket_);
-    //read operation
-      string message = read_(socket_);
-      cout << message << endl;
-    //write operation
-      send_(socket_, "HTTP/1.1 200 OK\r\n\r\n");
-    socket_.close();
-   return 0;
+    std::vector<std::thread> request_threads;
+
+    boost::asio::io_service io_service;
+    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 3005));
+
+    while (true) {
+        // Inicializar um novo socket a cada iteração
+        tcp::socket socket_(io_service);
+        // acceptor usa o socket para receber uma request
+        acceptor_.accept(socket_);
+        // cria um thread para lidar com a requisição
+        /*thread é passado como um movable pq ao final do escopo vai ser
+         *deletado, então precisa manter o lifetime dele*/
+        // Da um detach pra destruir dps que a função termina
+        request_threads
+            .emplace_back(
+                [](tcp::socket socket_) {
+                    RequestHandler handler(std::move(socket_));
+                    handler.handle();
+                },
+                std::move(socket_))
+            .detach();
+    }
+
+    return 0;
 }
