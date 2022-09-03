2fd074867602edb564f5fec800ff7fc9dcc74ef0
GedorN <gedor.silvaneto@gmail.com>
2022-09-03 18:22:59 -0300

    Parse das requests

--- server.cpp
+++ server.cpp
@@ -12,26 +12,122 @@
 #include <thread>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <future>
+#include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string/regex.hpp>
+#include <functional>
+#include <unordered_map>
+#include <algorithm>
 
-namespace beast = boost::beast;  // from <boost/beast.hpp>
-namespace http = beast::http;    // from <boost/beast/http.hpp>
-namespace net = boost::asio;     // from <boost/asio.hpp>
-using tcp = net::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast; // from <boost/beast.hpp>
+namespace http = beast::http;   // from <boost/beast/http.hpp>
+namespace net = boost::asio;    // from <boost/asio.hpp>
+using tcp = net::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 static int request_counter = 0;
 
-class RequestHandler {
-   private:
-    tcp::socket socket_;
+std::vector<std::string> split(const std::string& s, char delimiter) {
+  int index = 0;
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
 
-   public:
+  if(tokens.size() == 3) {
+    tokens[1] = tokens[1] + ":" + tokens[2];
+    tokens.pop_back();
+  }
+
+  return tokens;
+}
+
+class RequestHandler
+{
+private:
+    tcp::socket socket_;
+    std::vector<std::string> tokens;
+    std::map<std::string, std::string> headers;
+    std::vector<std::string> accepted_headers;
+    std::string public_path;
+    std::string body;
+
+public:
     // Recebe o socket(que é um objeto que foi movido), e move novamente.
     // Dessa vez move para ser um objeto persistente desse objeto(this)
-    RequestHandler(tcp::socket socket) : socket_(std::move(socket)) {}
+    RequestHandler(tcp::socket socket) : socket_(std::move(socket)),
+    public_path{"./public"}
+    {
+        accepted_headers.emplace_back("Content-Type");
+        accepted_headers.emplace_back("Accept");
+        accepted_headers.emplace_back("Host");
+    }
 
-    void handle() {
+    void handle()
+    {
         std::string message = read_(socket_);
         std::cout << message << std::endl;
+        boost::split_regex(tokens, message, boost::regex("(\r\n)+"));
+        if (tokens.empty())
+        {
+            send_(socket_, "HTTP/1.0 400 Bad Request\r\n\r\nSE FUDEU: " +
+                               std::to_string(request_counter));
+            socket_.close();
+            return;
+        }
+
+        std::vector<std::string> first_line;
+        boost::split(first_line, tokens[0], boost::is_any_of(" "));
+
+        std::string method = first_line[0];
+        std::cout << "veja o método: " << method << std::endl;
+        std::cout << "Tokens Size: " << tokens.size() << std::endl;
+        
+
+        for(int i = 1;  i < tokens.size(); i++)
+        {
+            std::vector<std::string> header_tokens;
+            header_tokens = split(tokens[i], ':');
+            if(header_tokens.size() == 1)
+            {
+                // TODO! O body está vindo sem \n, porque na linha 73, substituimos todos os \r\n por VAZIO. Então se vier um texto multilinha, não vai funcionar.
+                for(int j = i; j < tokens.size(); j++)
+                {
+                    body += tokens[j] + "\r\n";
+                }
+                break;
+            }
+            //boost::split_regex(header_tokens, tokens[i], boost::regex("^.+?(?=\:)"));
+            std::cout << "demonio: " << tokens[i] << std::endl;
+            boost::trim(header_tokens[0]);
+            boost::trim(header_tokens[1]);
+            std::cout << "TO vendo o resultado: " << header_tokens[1] << std::endl;
+
+            if(header_tokens.size() != 2) continue;
+            std::cout << "TO vendo o token: " << header_tokens[0] << std::endl;
+
+            std::string header = header_tokens[0];
+            if(std::find(accepted_headers.begin(), accepted_headers.end(), header) != accepted_headers.end())
+            {
+                std::cout << "Aceitei essa safada " << header_tokens[0] << std::endl;
+                std::string value = header_tokens[1];
+                headers[header] = value;
+                std::cout << "Atribui nessa safada " << header_tokens[1] << std::endl;
+            }
+        }
+        
+        std::cout<< "Body: " << body << std::endl;
+
+        // if (method == "GET"){
+        //     handle_get_request();
+        // }
+        // else if (method == "POST"){
+        //     handle_post_request();
+
+        // }
 
         send_(socket_, "HTTP/1.1 200 OK\r\n\r\nTESTE: " +
                            std::to_string(request_counter));
@@ -39,26 +135,44 @@ class RequestHandler {
         socket_.close();
     }
 
-    std::string read_(tcp::socket& socket) {
+    std::string read_(tcp::socket &socket)
+    {
         boost::asio::streambuf buf;
         boost::asio::read_until(socket, buf, "\n");
-        std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+        std::string data = boost::asio::buffer_cast<const char *>(buf.data());
         return data;
     }
 
-    void send_(tcp::socket& socket, const std::string& message) {
+    void send_(tcp::socket &socket, const std::string &message)
+    {
         const std::string msg = message + "\n";
         boost::asio::write(socket, boost::asio::buffer(message));
     }
+
+    void handle_get_request()
+    {
+        // std::string path = tokens[index++];
+        // std::cout << "GET " << path << std::endl;
+        
+    }
+
+    void handle_post_request() {
+        // std::string path = tokens[index+=2];
+        // std::cout << "POST" << path << std::endl;
+
+        
+    }
 };
 
-int main() {
-    std::vector<std::thread> request_threads;
+int main()
+{
+    // std::vector<std::thread> request_threads;
 
     boost::asio::io_service io_service;
     tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 3005));
 
-    while (true) {
+    while (true)
+    {
         // Inicializar um novo socket a cada iteração
         tcp::socket socket_(io_service);
         // acceptor usa o socket para receber uma request
@@ -67,14 +181,16 @@ int main() {
         /*thread é passado como um movable pq ao final do escopo vai ser
          *deletado, então precisa manter o lifetime dele*/
         // Da um detach pra destruir dps que a função termina
-        request_threads
-            .emplace_back(
-                [](tcp::socket socket_) {
-                    RequestHandler handler(std::move(socket_));
-                    handler.handle();
-                },
-                std::move(socket_))
-            .detach();
+        std::async(
+            std::launch::async,
+            [](tcp::socket socket_)
+            {
+                RequestHandler handler(std::move(socket_));
+                handler.handle();
+            },
+            std::move(socket_));
+
+        // .detach();
     }
 
     return 0;
